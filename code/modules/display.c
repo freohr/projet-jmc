76fbdb3a8e98ccbf477586f8e32110ed8b91d1c7
léo lefebvre <leo-lefebvre@hotmail.fr>
2013-02-26 21:29:56 +0100

    get et set fait pour display

--- code/modules/display.c
+++ code/modules/display.c
@@ -1,1 +1,83 @@
+#include "display.h"
 
+void initialize_SDL()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+}
+
+
+void free_SDL()
+{
+    SDL_Quit();
+}
+
+/* get */
+
+SDL_Rect get_dDefinition (const display *disp)
+{
+    return disp->definition;
+}
+
+SDL_Surface* get_dScreen (const display *disp)
+{
+    return disp->screen;
+}
+
+sprite* get_dCharacter (const display *disp)
+{
+    return disp->character;
+}
+
+sprite* get_dPlatform (const display *disp)
+{
+    return disp->platform;
+}
+
+SDL_Surface* get_sImage (const sprite *sp)
+{
+    return sp->image;
+}
+
+SDL_Rect get_sPosition (const sprite *sp)
+{
+    return sp->position;
+}
+
+/* set */
+
+void set_dDefiniton (display *disp, int height, int width)
+{
+    disp->definition.y=height;
+    disp->definition.x=width;
+}
+
+void set_dScreen (display *disp, int height, int width, int format, char *mode)
+{
+    disp->screen = SDL_SetVideoMode(height, width, format, mode);
+}
+
+void set_dCharacter (display *disp, char *path, int height, int width)
+{
+    set_sImage(disp->character, path);
+}
+
+void set_dPlatform (display *disp, char *path, int height, int width)
+{
+    set_sImage(disp->platform, path);
+}
+
+void set_sImage (sprite *sp, char *path)
+{
+    sp->image = SDL_LoadBMP(path);
+}
+
+void set_sPosition (sprite *sp, int height, int witdh)
+{
+    sp->position.y = height;
+    sp->position.x = witdh;
+}
+
+int convert_position_obj (int height_screen, int obj_position, int screen_position, int height_obj)
+{
+    return height_screen - (obj_position - screen_position) - height_obj;
+}
--- code/modules/display.h
+++ code/modules/display.h
@@ -6,20 +6,62 @@
 #include <SDL/SDL.h>
 #include"struct.h"
 
-struct sScreen
+struct sSprite
 {
-	coordinate definiton; /* taille de l'écran */
-	plan plans[];
-	int nb_plans;
-}
-typedef struct sScreen screen
+    SDL_Surface *image;
+    SDL_Rect position;
+};
+typedef struct sSprite sprite;
 
-struct sPlan
+struct sDisplay
 {
-	SDL_Surface sprite;
-	int nb_images;
-	int max_images;
-}
-typedef struct sPlan plan
+	SDL_Rect definition; /* taille de l'écran */
+	SDL_Surface *screen;
+	sprite *character;
+	sprite *platform;
+};
+typedef struct sDisplay display;
+
+void initialize_SDL();
+display* initialize_display();
+sprite* initialize_sprite();
+
+void free_SDL();
+void free_display();
+void free_sprite();
+
+/* get */
+SDL_Rect get_dDefinition (const display *disp);
+SDL_Surface* get_dScreen (const display *disp);
+sprite* get_dCharacter (const display *disp);
+sprite* get_dPlatform (const display *disp);
+SDL_Surface* get_sImage (const sprite *sp);
+SDL_Rect get_sPosition (const sprite *sp);
+
+/* set */
+void set_dDefiniton (display *disp, int height, int width);
+void set_dScreen (display *disp, int height, int width, int format, char *mode);
+void set_dCharacter (display *disp, char *path, int height, int width);
+/* précondition :   - disp initialisé
+                    - path contient le chemin relatif de l'image
+                    - weight et width contiennent la position de l'image */
+void set_dPlatform (display *disp, char *path, int height, int width);
+/* précondition :   - disp initialisé
+                    - path contient le chemin relatif de l'image
+                    - weight et width contiennent la position de l'image */
+void set_sImage (sprite *sp, char *path);
+/* précondition :   - sp initialisé
+                    - path contient le chemin relatif de l'image */
+
+void set_sPosition (sprite *sp, int height, int witdh);
+/* précondition :   - disp initialisé
+                    - weight et width contiennent la position de l'image */
+
+
+int convert_position_obj (int height_screen, int obj_position, int screen_position, int height_obj);
+/* la SDL considére que le point (0,0) est en haut à gauche de l'écran
+tandis que notre moteur considére que le point (0,0) est en bas à droite
+du niveau, cette fonction convertis une hauteur envoyée par le moteur
+en une hauteur utilisable pour la SDL */
 
 #endif
