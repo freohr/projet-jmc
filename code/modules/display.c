f33a846a2c49d9dc003a5a6be8b8c242d08117c2
léo lefebvre <leo-lefebvre@hotmail.fr>
2013-02-26 23:51:51 +0100

    mise a jour de display.c et display.h
    Initilisalitions et liberations terminées

--- code/modules/display.c
+++ code/modules/display.c
@@ -5,20 +5,60 @@ void initialize_SDL()
     SDL_Init(SDL_INIT_VIDEO);
 }
 
-display* initialize_display(int height, int width)
+display* initialize_display(int height, int width, int format, char *mode, char *window_name)
 {
     display *disp;
     disp = malloc(sizeof(display));
-    disp->definition.y = height;
-    disp->definition.x = width;
-    disp->character = NULL;
-    disp->platform = NULL;
-    disp->screen
+    set_dDefiniton (disp, height, width); /* initialisation de la taille */
+    set_dScreen (disp, height, width, format, mode); /* initialise l'écran */
+    SDL_WM_SetCaption(window_name, NULL); /* met le nom donné en paramètre à la fenètre */
+    disp->character = initialize_sprite();
+    disp->platform = initialize_sprite();
+    return disp;
+}
+
+sprite* initialize_sprite()
+{
+    sprite *sp;
+    sp = malloc(sizeof(sprite));
+    sp->image = NULL;
+    set_sPosition (sp, 0, 0);
+    return sp;
+}
+
+void initialize_display_module()
+{
+    display *disp;
+    initialize_SDL();
+    disp = initialize_display(640, 480, 32, "SDL_HWSURFACE | SDL_DOUBLEBUF", "prototype");
+    SDL_FillRect(disp->screen, NULL, SDL_MapRGB(disp->screen->format, 0, 0, 0)); /* initialise l'écran en noir */
 }
 
 void free_SDL()
 {
     SDL_Quit();
+}
+
+void free_display(display *disp)
+{
+    set_dDefiniton (disp, 0, 0);
+    SDL_FreeSurface(disp->screen);
+    free_sprite(disp->character);
+    free_sprite(disp->platform);
+    free(disp);
+}
+
+void free_sprite(sprite *sp)
+{
+    SDL_FreeSurface(sp->image);
+    set_sPosition (sp, 0, 0);
+    free(sp);
+}
+
+void free_display_module(display *disp)
+{
+    free_display(disp);
+    free_SDL();
 }
 
 /* get */
@@ -68,17 +108,22 @@ void set_dScreen (display *disp, int height, int width, int format, char *mode)
 
 void set_dCharacter (display *disp, char *path, int height, int width)
 {
-    set_sImage(disp->character, path);
+    if (path == NULL)
+        set_sImage(disp->character, path);
+    set_sPosition(disp->character, height, width);
 }
 
 void set_dPlatform (display *disp, char *path, int height, int width)
 {
-    set_sImage(disp->platform, path);
+    if (path == NULL)
+        set_sImage(disp->platform, path);
+    set_sPosition(disp->character, height, width);
 }
 
 void set_sImage (sprite *sp, char *path)
 {
-    sp->image = SDL_LoadBMP(path);
+    if (path == NULL)
+        sp->image = SDL_LoadBMP(path);
 }
 
 void set_sPosition (sprite *sp, int height, int witdh)
--- code/modules/display.h
+++ code/modules/display.h
@@ -22,18 +22,24 @@ struct sDisplay
 };
 typedef struct sDisplay display;
 
+/* fonctions d'initialisation  et de libérations */
+
 void initialize_SDL();
-display* initialize_display(int height, int width, char format, char *mode, char *window_name);
+display* initialize_display(int height, int width, int format, char *mode, char *window_name);
 /* précondition :   - weight et width contiennent la taille de l'écran
                     - format contient le format de codage des pixel (mettre a 32)
                     - mode est le mode d'affichage de l'écran
-   postcondition : display initialisé */
+                    - window_name est le nom a donner a la fenêtre
+   postcondition : display initialisé et screen contient une image noire de la taille de l'écran*/
 
 sprite* initialize_sprite();
+/* postcondition : display initialisé et screen contient une image noire de la taille de l'écran*/
+void initialize_display_module();
 
 void free_SDL();
-void free_display();
-void free_sprite();
+void free_display(display *disp);
+void free_sprite(sprite *sp);
+void free_display_module(display *disp);
 
 /* get */
 SDL_Rect get_dDefinition (const display *disp);
@@ -54,19 +60,21 @@ void set_dScreen (display *disp, int height, int width, int format, char *mode);
 void set_dCharacter (display *disp, char *path, int height, int width);
 /* précondition :   - disp initialisé
                     - path contient le chemin relatif de l'image
+                    - si path est NULL, l'image ne sera pas changée
                     - weight et width contiennent la position de l'image */
 void set_dPlatform (display *disp, char *path, int height, int width);
 /* précondition :   - disp initialisé
                     - path contient le chemin relatif de l'image
+                    - si path est NULL, l'image ne sera pas changée
                     - weight et width contiennent la position de l'image */
 void set_sImage (sprite *sp, char *path);
 /* précondition :   - sp initialisé
-                    - path contient le chemin relatif de l'image */
+                    - path contient le chemin relatif de l'image
+                    - si path est NULL, l'image ne sera pas changée*/
 
 void set_sPosition (sprite *sp, int height, int witdh);
 /* précondition :   - disp initialisé
                     - weight et width contiennent la position de l'image */
-
 
 int convert_position_obj (int height_screen, int obj_position, int screen_position, int height_obj);
 /* la SDL considére que le point (0,0) est en haut à gauche de l'écran
